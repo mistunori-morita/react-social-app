eb197e3145ae1f265a9bfdfffc9a15888fca9836
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-04-25 18:35:59 +0900

    modify package.json, Facebook Loginhttps setting

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { Route, Router } from 'react-router'
 import PropTypes from 'prop-types'
 import './App.css'
-
 import Home from './views/Home'
 import Login from './views/Login'
 
