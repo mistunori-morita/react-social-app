02b3a91bfc5364b8632dcbe4fd28f44b57a85a56
mitsunori.morita <mitsunori.morita@speee.jpmitsunori.morita@speee.jpmitsunori.morita@speee.jp>
2018-04-26 18:29:45 +0900

    Navbar html add

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,17 @@ class Home extends Component {
   render(){
     return(
       <div>
-        <h2>Home</h2>
+        <nav>
+          <div className="nav-wrapper">
+            <a href="#" className="brand-logo right">Logo</a>
+            <ul id="nav-mobile" className="left hide-on-med-and-down">
+              <li><a href="sass.html">Sass</a></li>
+              <li><a href="badges.html">Components</a></li>
+              <li><a href="collapsible.html">JavaScript</a></li>
+            </ul>
+          </div>
+        </nav>
+
       </div>
     );
   }
